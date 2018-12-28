f1f8a6c28b7ae7a6bd10954a273619e34bceb2d7
emin.roblack@gmail.com <emin.roblack@gmail.com>
2018-12-28 18:36:00 +0100

    Add Pods

--- NewsReader_demo/ViewController.swift
+++ NewsReader_demo/ViewController.swift
@@ -5,18 +5,35 @@
 //  Created by Emin Roblack on 12/28/18.
 //  Copyright © 2018 emiN Roblack. All rights reserved.
 //
+    // https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=ad27cf29007846648b1d77ac7c5b41f1
+
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
+  
+  let NewsUrl = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=ad27cf29007846648b1d77ac7c5b41f1"
 
   override func viewDidLoad() {
     super.viewDidLoad()
 
-    
-    // proradi jebem ti mater
   }
-
-
+  
+  
+  //MARK:- Getting the News ------------------------------------------------
+  
+  func getLatestNews(url: String) {
+    
+    Alamo
+    
+    
+  }
+  
+  
+  //------------------------------------------------------------------------
+  
+  
 }
 
---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, '12.0'
+
+target 'NewsReader_demo' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for NewsReader_demo
+  pod 'Alamofire'
+  pod 'SwiftyJSON', '~> 4.0'
+
+end
