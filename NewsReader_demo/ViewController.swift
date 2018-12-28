ebc46e407452c7b6e025932f1d3350ae62ab052c
emin.roblack@gmail.com <emin.roblack@gmail.com>
2018-12-28 19:48:47 +0100

    Add news test

--- NewsReader_demo/ViewController.swift
+++ NewsReader_demo/ViewController.swift
@@ -14,25 +14,41 @@ import SwiftyJSON
 
 class ViewController: UIViewController {
   
-  let NewsUrl = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=ad27cf29007846648b1d77ac7c5b41f1"
+  @IBOutlet weak var titleLabel: UILabel!
+  @IBOutlet weak var contentLabel: UILabel!
+  
+  
+  let newsUrl = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=ad27cf29007846648b1d77ac7c5b41f1"
 
   override func viewDidLoad() {
     super.viewDidLoad()
-
+    getLatestNews(url: newsUrl)
   }
   
   
   //MARK:- Getting the News ------------------------------------------------
-  
   func getLatestNews(url: String) {
     
-    Alamo
-    
-    
+    Alamofire.request(url, method: .get, parameters: nil).responseJSON {
+      response in
+      
+      if response.result.isSuccess {
+        print("We are a GO")
+        
+        let newsJSON: JSON = JSON(response.result.value ?? "default")
+        self.updateLabels(json: newsJSON)
+        print(newsJSON)
+      } else {
+        print("We got an error: \(response.result.error!)")
+      }
+    }
   }
-  
-  
   //------------------------------------------------------------------------
+  
+  func updateLabels(json: JSON) {
+    titleLabel.text = json["articles"][0]["title"].stringValue
+    contentLabel.text = json["articles"][0]["description"].stringValue
+  }
   
   
 }
