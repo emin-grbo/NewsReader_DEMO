7bd82f1e544e5d27262e0393b8089a7e2b9c061e
emin.roblack@gmail.com <emin.roblack@gmail.com>
2018-12-29 11:59:51 +0100

    fix image preview

--- NewsReader_demo/ViewController.swift
+++ NewsReader_demo/ViewController.swift
@@ -5,7 +5,7 @@
 //  Created by Emin Roblack on 12/28/18.
 //  Copyright © 2018 emiN Roblack. All rights reserved.
 //
-    // https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=ad27cf29007846648b1d77ac7c5b41f1
+// https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=ad27cf29007846648b1d77ac7c5b41f1
 
 
 import UIKit
@@ -36,6 +36,7 @@ class ViewController: UIViewController {
       if response.result.isSuccess {
         print("We are a GO")
         
+        // Get the json from results, and assign it to a variable
         let newsJSON: JSON = JSON(response.result.value ?? "default")
         self.updateLabels(json: newsJSON)
         print(newsJSON)
@@ -46,20 +47,25 @@ class ViewController: UIViewController {
   }
   //------------------------------------------------------------------------
   
+  
+  //MARK:- Update view ------------------------------------------------
   func updateLabels(json: JSON) {
     titleLabel.text = json["articles"][0]["title"].stringValue
     contentLabel.text = json["articles"][0]["description"].stringValue
-    
     let imageURL = json["articles"][0]["urlToImage"].stringValue
-
+    
+    // Get image from them internets
     Alamofire.request(imageURL).responseData {
       response in
       if let image = response.result.value {
         let image = UIImage(data: image)
+        // show thy image
         self.imageView.image = image
       }
     }
   }
+  //------------------------------------------------------------------------
+
   
   
 }
